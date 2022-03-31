b4b602b7bc91cbac9dc8a20f6eccf44cdd9772b4
Jazz-L <jazmyneleathers@gmail.com>
2022-03-31 16:02:42 -0500

    Style: Added grid height and grid width variables for user input

--- designs.js
+++ designs.js
@@ -1,7 +1,8 @@
 // Select color input
 var gridColor = document.getElementsById('colorPicker');
 // Select size input
-
+var gridHeight = document.getElementsById('inputHeight');
+var gridWidth = document.getElementsById('inputWidth');
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
