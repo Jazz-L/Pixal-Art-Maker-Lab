13f15ae1ecb0e0e36b9f20c985d5635d8b44a658
Jazz-L <jazmyneleathers@gmail.com>
2022-04-01 17:50:44 -0500

    Style: Created newer grid function

--- designs.js
+++ designs.js
@@ -4,14 +4,14 @@ var gridColor = document.getElementsById('colorPicker');
 // Select size input
 var gridHeight = document.getElementsById('inputHeight');
 var gridWidth = document.getElementsById('inputWidth');
-const submitBtn = document.getElementsById('submitBtn');
-const grid = document.getElementsById('pixelCanvas');
+var grid = document.getElementsById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
-const height = gridHeight.value;
-const width = gridWidth.value;
 
+for (var i = 0; i < height.val; i++) {
+
+}
 // Your code goes here!
 
 }
