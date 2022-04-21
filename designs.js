70a17deeed58020cb110d790fad90241188eba03
Jazz-Leathers <104086589+Jazz-Leathers@users.noreply.github.com>
2022-04-21 14:23:19 -0500

    Trying to update submit button

--- designs.js
+++ designs.js
@@ -1,24 +1,24 @@
 // Select color input
 var gridColor = document.getElementById('colorPicker');
 // Select size input
-var gridHeight = document.getElementsById('inputHeight');
-var gridWidth = document.getElementsById('inputWidth');
-const grid = document.getElementsById('pixelCanvas');
-const submitButton = document.getElementById("submitBtn");
-
+var gridHeight = document.getElementById('inputHeight');
+var gridWidth = document.getElementById('inputWidth');
 
 
 // When size is submitted by the user, call makeGrid()
-submitButton.addEventListener('click', function (x) {
-    makeGrid();
-});
+const submitButton = document.getElementById("submitBtn");
+document.getElementById("submitBtn")
+const grid = document.getElementById('pixelCanvas');
 
-function makeGrid(x) {
-	for (var i = 0; i < gridHeight.value; i++) {
-		const row = grid.insertRow(0);
-		for (var j = 0; j < gridWidth.value; j++) {
-			row.insertCell(0);
-		}
-	}
-}
+function makeGrid(tr, td) {
+    for (var tr = 0; tr < height; tr++) {
+        for (var td = 0; td < width; td++)
+            ;};
+};
+
+// function that clears the grid
+function clearGrid() {
+    $("table").remove();
+};
+
 // Your code goes here!
