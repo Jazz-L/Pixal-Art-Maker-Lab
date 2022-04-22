e86f91232431e3d5ad1c393dce5ff80ed566253a
Jazz-Leathers <104086589+Jazz-Leathers@users.noreply.github.com>
2022-04-22 12:55:42 -0500

    Style: Final Product

--- designs.js
+++ designs.js
@@ -1,24 +1,49 @@
 // Select color input
-var gridColor = document.getElementById('colorPicker');
-// Select size input
-var gridHeight = document.getElementById('inputHeight');
-var gridWidth = document.getElementById('inputWidth');
+function addClickEventToCells() {
+    'use strict';
+    var colorPicker = document.getElementById("colorPicker");
+    var cells = document.getElementsByClassName('cell');
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("click", function (event) {
+            var clickedCell = event.target;
+            clickedCell.style.backgroundColor = colorPicker.value;
+        });
+    }
+}
 
+
+// Select size input
+
+function formSubmission() {
+    'use strict';
+    event.preventDefault();
+    var height = document.getElementById('inputHeight').value;
+    var width = document.getElementById('inputWidth').value;
+    makeGrid(height, width);
+}
 
 // When size is submitted by the user, call makeGrid()
-const submitButton = document.getElementById("submitBtn");
-document.getElementById("submitBtn")
-const grid = document.getElementById('pixelCanvas');
 
-function makeGrid(tr, td) {
-    for (var tr = 0; tr < height; tr++) {
-        for (var td = 0; td < width; td++)
-            ;};
+function makeGrid(n, m) {
+    'use strict';
+    // Your code goes here!
+
+    var table = document.getElementById("pixelCanvas");
+    var grid = '';
+    for (var i = 0; i < n; i++) {
+        grid += '<tr class="row-' + i + '">';
+        for (var j = 0; j < m; j++) {
+            grid += '<td class="cell" id="row-' + i + '_cell-' + j + '"></td>';
+        }
+        grid += '</tr>';
+    }
+
+    table.innerHTML = grid;
+    addClickEventToCells();
+}
+
+document.getElementById('sizePicker').onsubmit = function () {
+    formSubmission();
 };
 
-// function that clears the grid
-function clearGrid() {
-    $("table").remove();
-};
-
-// Your code goes here!
+makeGrid(10, 10);
